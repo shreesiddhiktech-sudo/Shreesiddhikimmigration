a9037bd22919888cc918075e7ce4d8ee9d974e06
Shi06vani <shivanisuryavanshi612@gmail.com>
2026-01-20 23:38:37 +0530

    pushe form code

--- src/routes/customer.routes.js
+++ src/routes/customer.routes.js
@@ -1,14 +1,1 @@
-const express = require("express");
-const router = express.Router();
-const controller = require("../controllers/customer.controller");
-const upload = require("../middlewares/customerUpload");
-const { validate } = require("../middlewares/validate.middleware");
-const { createCustomerSchema, updateCustomerSchema } = require("../validations/customer.validation");
-
-router.post("/customers", upload.single("picture"),  validate(createCustomerSchema), controller.create);
-router.get("/customers", controller.findAll);
-router.get("/customers/:id", controller.findOne);
-router.put("/customers/:id", upload.single("picture"), validate(updateCustomerSchema), controller.update);
-router.delete("/customers/:id", controller.remove);
-
-module.exports = router;
+kjs
