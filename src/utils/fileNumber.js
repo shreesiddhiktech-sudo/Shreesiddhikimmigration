331d8ab8d26a85075e5a69cf1e7eefca33d73b9d
Shi06vani <shivanisuryavanshi612@gmail.com>
2026-01-21 00:15:38 +0530

    api crete for visa for api

--- src/utils/fileNumber.js
+++ src/utils/fileNumber.js
@@ -1,5 +1,2 @@
 exports.generateFileNumber = (visaType) => {
-  const prefix = visaType === "study" ? "STD" : "WRK";
-  const random = Math.floor(100000000 + Math.random() * 900000000); // 9 digit
-  return `${prefix}${random}`;
-};
+  const prefix
